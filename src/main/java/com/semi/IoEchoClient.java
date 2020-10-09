4960d276f370afae74236b47f1354763f3aed279
sulaymon <demirel6777@gmail.com>
2020-10-09 23:13:15 +0300

    implemented functionality connect multiple clients to server and send and receive messages

--- src/main/java/com/semi/IoEchoClient.java
+++ src/main/java/com/semi/IoEchoClient.java
@@ -1,5 +1,6 @@
 package com.semi;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,16 +14,59 @@ public class IoEchoClient {
         InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
 
-        String test = "test";
-        outputStream.write(test.getBytes());
-        while (true) {
+        IoEchoClient ioEchoClient = new IoEchoClient();
+        ioEchoClient.readFromServer(inputStream);
+        ioEchoClient.writeToServer(outputStream);
 
-            int read;
-            byte [] bytes = new byte[3];
-            while ((read = inputStream.read(bytes)) != -1) {
-                System.out.println("byte: " + read + " bytes: " + new String(bytes));
+        socket.close();
+
+    }
+
+    public void readFromServer(InputStream inputStream) {
+        new MsgReader(inputStream);
+    }
+
+    public void writeToServer (OutputStream outputStream) {
+        InputStream inputFromConsole = new BufferedInputStream(System.in);
+        try {
+            while (true) {
+
+                int read;
+                byte[] bytes = new byte[4];
+                while ((read = inputFromConsole.read(bytes)) != -1) {
+                    String input = new String(bytes);
+                    System.out.println("byte: " + read + " bytes: " + input);
+                    outputStream.write(input.getBytes());
+                    if (input.toLowerCase().equals("exit")) {
+                        break;
+                    }
+                }
             }
-            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    class MsgReader extends Thread {
+
+        private final InputStream inputStream;
+        public MsgReader (InputStream inputStream) {
+            super();
+            this.inputStream = inputStream;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                byte[] bytes = new byte[2];
+                while (inputStream.read(bytes) != 1) {
+                    System.out.println("Msg from server: " + new String(bytes));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/main/java/com/semi/IoEchoServer.java
+++ src/main/java/com/semi/IoEchoServer.java
@@ -12,26 +12,60 @@ public class IoEchoServer {
         int port = 2020;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Started server on port " + port);
+        IoEchoServer echoServer = new IoEchoServer();
 
         while (true) {
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            int read;
-            byte[] bytes = new byte[2];
-            while ((read = inputStream.read(bytes)) != -1) {
-                System.out.println(read);
-                outputStream.write(bytes, 0, read);
+            try {
+                Socket socket = serverSocket.accept();
+                echoServer.addNewClient(socket);
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
             }
-            System.out.println(read);
-
-            socket.close();
-            break;
         }
 
         serverSocket.close();
     }
+
+    public void addNewClient (Socket socket) {
+        Client client = new Client(socket);
+        System.out.println("Connected new client with id: " + socket.getRemoteSocketAddress().toString());
+    }
+
+    class Client extends Thread {
+
+        private final Socket socket;
+        public Client(Socket socket) {
+            super();
+            this.socket = socket;
+            start();
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+
+                int read;
+                byte[] bytes = new byte[2];
+                while ((read = inputStream.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
 
